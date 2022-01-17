992a16a8cdc7cf7e2989166bd01296134554feb0
Readpato <87768087+Readpato@users.noreply.github.com>
2022-01-17 12:49:21 +0100

    feat: Create function that handles the errors
    The function that displays and removes errors has been added to the project. This is needed to prevent errors during the execution of the webpage

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ const $convertButton = document.querySelector(".convert-button");
 const $dateInput = document.querySelector("#date-input");
 const $amountInput = document.querySelector("#amount-input");
 const $convertionResultBar = document.querySelector(".convertion-result-bar");
+const $form = document.form;
 
 function getApiCurrencies(API_URL) {
   return fetch(API_URL)
@@ -33,13 +34,79 @@ function getApiCurrencies(API_URL) {
     .catch((error) => console.error(error));
 }
 
-$convertButton.onclick = () => {
-  let baseCurrency = $baseCurrencySelector.value;
-  let expectedCurrency = $expectedCurrencySelector.value;
-  let amount = $amountInput.value;
+$form.onsubmit = validateForm;
 
-  convertCurrency(baseCurrency, expectedCurrency, amount);
-};
+function validateForm(event) {
+  event.preventDefault();
+  const amountValue = $form["amount-input-value"].value;
+  const baseCurrencyvalue = $form["base-currency-value"].value;
+  const expectedCurrencyValue = $form["expected-currency-value"].value;
+
+  const errors = {
+    "amount-input-value": validateAmount(amountValue),
+    "base-currency-value": validateBaseCurrency(baseCurrencyvalue),
+    "expected-currency-value": validateExpectedCurrency(expectedCurrencyValue),
+  };
+
+  deleteOldErrors();
+
+  const success = handleErrors(errors) === 0;
+
+  if (success) {
+    convertCurrency(baseCurrencyvalue, expectedCurrencyValue, amountValue);
+  }
+}
+
+function handleErrors(errors) {
+  const error = errors;
+  const keys = Object.keys(errors);
+  let errorQuantity = 0;
+
+  keys.forEach(function (key) {
+    if (error[key]) {
+      $form[key].classList.add("error");
+      $form[key].value = "";
+
+      const $errorList = document.querySelector(".existing-errors");
+      const $error = document.createElement("li");
+      $error.textContent = error[key];
+      $error.classList.add("existing-error");
+      $errorList.appendChild($error);
+      errorQuantity++;
+    } else {
+      $form[key].classList.remove("error");
+    }
+  });
+  return errorQuantity;
+}
+
+function deleteOldErrors() {
+  const $existingErrors = document.querySelectorAll(".existing-error");
+  $existingErrors.forEach((error) => {
+    error.remove();
+  });
+}
+
+function validateAmount(amountValue) {
+  const regEx = /^[0-9]+$/;
+
+  if (amountValue === "") return "Please, insert an amount.";
+  if (!regEx.test(amountValue)) return "The amount field only accepts numbers";
+
+  return "";
+}
+
+function validateBaseCurrency(baseCurrencyValue) {
+  if (baseCurrencyValue === "") return "Please, insert a base currency.";
+  return "";
+}
+
+function validateExpectedCurrency(expectedCurrencyValue) {
+  if (expectedCurrencyValue === "")
+    return "Please, insert an expected currency.";
+
+  return "";
+}
 
 function convertCurrency(baseCurrency, expectedCurrency, amount) {
   return fetch(API_URL.replace("USD", `${baseCurrency}`))
@@ -87,45 +154,6 @@ function updateResultStatus(
   amount
 ) {
   return ($convertionResultBar.textContent = `${amount} ${baseCurrency} is equal to ${convertionResult.toString()} ${expectedCurrency}`);
-}
-
-function validateForm(event) {
-  event.preventDefault();
-  const $form = document.form;
-  const amountValue = $form["amount-input-value"].value;
-  const baseCurrencyvalue = $form["base-currency-value"].value;
-  const expectedCurrencyValue = $form["expected-currency-value"].value;
-
-  const errors = {
-    "amount-value": validateAmount(amountValue),
-    "base-currency-value": validateBaseCurrency(baseCurrencyvalue),
-    "expected-Currency-Value": validateExpectedCurrency(expectedCurrencyValue),
-  };
-
-  const success = handleErrors(errors) === 0;
-
-  if (success) return convertCurrency(/*  Insert values here */);
-}
-
-function validateAmount(amountValue) {
-  const regEx = /^[0-9]+$/;
-
-  if (amountValue === "") return "Please, insert an amount.";
-  if (!regEx.test(amountValue)) return "The amount field only accepts numbers";
-
-  return true;
-}
-
-function validateBaseCurrency(baseCurrencyValue) {
-  if (baseCurrencyValue === "") return "Please, insert a base currency.";
-  return true;
-}
-
-function validateExpectedCurrency(expectedCurrencyValue) {
-  if (expectedCurrencyValue === "")
-    return "Please, insert an expected currency.";
-
-  return true;
 }
 
 getApiCurrencies(API_URL);
