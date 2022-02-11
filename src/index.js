2e6e97eb7d6a2fd684b61d112792bd0cd960deff
Readpato <87768087+Readpato@users.noreply.github.com>
2022-02-11 09:43:48 +0100

    refactor: Rewrite fetch functions and optimize code
    The fetch functions have been rewritten to utilize async/await functions. Now, understanding how the code runs is simpler. Then, individual pieces of code that could have been a function on their own have also been rewritten.

--- src/index.js
+++ src/index.js
@@ -1,36 +1,29 @@
-const API_URL =
+async function initialize() {
+  const API_URL =
   "https://v6.exchangerate-api.com/v6/cb6cded1588a31eb0fda44c4/latest/USD";
-const $baseCurrencySelector = document.querySelector("#base-currency-selector");
-const $expectedCurrencySelector = document.querySelector(
-  "#expected-currency-selector"
-);
-const $convertionResultBar = document.querySelector(".convertion-result-bar");
-const $form = document.form;
-
-function getApiCurrencies(API_URL) {
-  return fetch(API_URL)
-    .then((apiResponse) => {
-      if (!apiResponse.ok) {
-        return "Something went wrong, please try again later.";
-      }
-      return apiResponse.json();
-    })
-    .then((apiResponseJSON) => {
-      let currencies = Object.keys(apiResponseJSON.conversion_rates);
-      return currencies.forEach((coin) => {
-        let $optionBaseCurrency = document.createElement("option");
-        let $optionExpectedCurrency = document.createElement("option");
-        $optionBaseCurrency.textContent = coin;
-        $optionBaseCurrency.value = coin;
-        $optionExpectedCurrency.textContent = coin;
-        $optionExpectedCurrency.value = coin;
-        $baseCurrencySelector.appendChild($optionBaseCurrency);
-        $expectedCurrencySelector.appendChild($optionExpectedCurrency);
-      });
-    })
-    .catch((error) => console.error(error));
+  const response = await fetch(API_URL);
+  const json = await response.json();
+  return addCurrencyLists(json);
 }
 
+function addCurrencyLists(json){
+  const $baseCurrencySelector = document.querySelector("#base-currency-selector");
+  const $expectedCurrencySelector = document.querySelector(
+    "#expected-currency-selector"
+  );
+  let currencies = Object.keys(json.conversion_rates);
+  return currencies.forEach((coin) => {
+    let $optionBaseCurrency = document.createElement("option");
+    let $optionExpectedCurrency = document.createElement("option");
+    $optionBaseCurrency.textContent = coin;
+    $optionBaseCurrency.value = coin;
+    $optionExpectedCurrency.textContent = coin;
+    $optionExpectedCurrency.value = coin;
+    $baseCurrencySelector.appendChild($optionBaseCurrency);
+    $expectedCurrencySelector.appendChild($optionExpectedCurrency);
+  });
+}
+const $form = document.form;
 $form.onsubmit = validateForm;
 
 function validateForm(event) {
@@ -105,32 +98,14 @@ function validateExpectedCurrency(expectedCurrencyValue) {
   return "";
 }
 
-function convertCurrency(baseCurrency, expectedCurrency, amount) {
-  return fetch(API_URL.replace("USD", `${baseCurrency}`))
-    .then((apiResponse) => {
-      if (!apiResponse.ok) {
-        return "Something went wrong, please try again later";
-      }
-      return apiResponse.json();
-    })
-    .then((apiResponseJSON) => {
-      let conversionRates = apiResponseJSON.conversion_rates;
-      return calculateConvertion(
-        conversionRates,
-        baseCurrency,
-        expectedCurrency,
-        amount
-      );
-    })
-    .then((convertionResult) => {
-      return updateResultStatus(
-        baseCurrency,
-        expectedCurrency,
-        convertionResult,
-        amount
-      );
-    })
-    .catch((error) => console.error(error));
+async function convertCurrency(baseCurrency, expectedCurrency, amount) {
+  const API_URL =
+  "https://v6.exchangerate-api.com/v6/cb6cded1588a31eb0fda44c4/latest/USD";
+  const response =  await fetch(API_URL.replace("USD", `${baseCurrency}`));
+  const json = await response.json();
+  const {conversion_rates: rates} = json;
+  const convertionResult = calculateConvertion(rates, baseCurrency, expectedCurrency, amount);
+  return updateResultStatus(baseCurrency, expectedCurrency, convertionResult, amount);
 }
 
 function calculateConvertion(
@@ -151,7 +126,8 @@ function updateResultStatus(
   convertionResult,
   amount
 ) {
+  const $convertionResultBar = document.querySelector(".convertion-result-bar");
   return ($convertionResultBar.textContent = `${amount} ${baseCurrency} is/are equal to ${convertionResult.toString()} ${expectedCurrency}`);
 }
 
-getApiCurrencies(API_URL);
+initialize();
