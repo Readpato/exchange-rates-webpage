4872027c7117a86e4c260596ab20787dae3a47ef
Readpato <87768087+Readpato@users.noreply.github.com>
2022-02-13 10:59:09 +0100

    refactor: Refactor service functions and create initialize function
    The service functions have been added a try and catch statement which will now display an error if something appears happens. Also, the app will now start when the initialize function is run at the index file.

--- src/index.js
+++ src/index.js
@@ -1,14 +1,11 @@
-import { addCurrencyLists } from "./ui.js";
+import { callAPI } from "./service.js";
 import { validateForm } from "./form-validations.js";
 
 const $form = document.form;
 $form.onsubmit = validateForm;
 
-async function initialize() {
-  const API_URL =
-    "https://v6.exchangerate-api.com/v6/cb6cded1588a31eb0fda44c4/latest/USD";
-  const response = await fetch(API_URL);
-  const json = await response.json();
-  return addCurrencyLists(json);
+function initialize() {
+  return callAPI();
 }
+
 initialize();
--- src/service.js
+++ src/service.js
@@ -1,22 +1,38 @@
 import { calculateConvertion } from "./logic.js";
-import { updateResultStatus } from "./ui.js";
+import { updateResultStatus, addCurrencyLists } from "./ui.js";
+
+export async function callAPI() {
+  try {
+    const API_URL =
+      "https://v6.exchangerate-api.com/v6/cb6cded1588a31eb0fda44c4/latest/USD";
+    const response = await fetch(API_URL);
+    const json = await response.json();
+    return addCurrencyLists(json);
+  } catch {
+    return console.error(error);
+  }
+}
 
 export async function convertCurrency(baseCurrency, expectedCurrency, amount) {
-  const API_URL =
-    "https://v6.exchangerate-api.com/v6/cb6cded1588a31eb0fda44c4/latest/USD";
-  const response = await fetch(API_URL.replace("USD", `${baseCurrency}`));
-  const json = await response.json();
-  const { conversion_rates: rates } = json;
-  const convertionResult = calculateConvertion(
-    rates,
-    baseCurrency,
-    expectedCurrency,
-    amount
-  );
-  return updateResultStatus(
-    baseCurrency,
-    expectedCurrency,
-    convertionResult,
-    amount
-  );
+  try {
+    const API_URL =
+      "https://v6.exchangerate-api.com/v6/cb6cded1588a31eb0fda44c4/latest/USD";
+    const response = await fetch(API_URL.replace("USD", `${baseCurrency}`));
+    const json = await response.json();
+    const { conversion_rates: rates } = json;
+    const convertionResult = calculateConvertion(
+      rates,
+      baseCurrency,
+      expectedCurrency,
+      amount
+    );
+    return updateResultStatus(
+      baseCurrency,
+      expectedCurrency,
+      convertionResult,
+      amount
+    );
+  } catch {
+    return console.error(error);
+  }
 }
